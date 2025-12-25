f5c6c25201e9207fd9afeae27a3dde25cfb34166
jerusalem-M <jerusalemmoges1111@gmail.com>
2025-12-25 23:47:32 +0300

    persist last show tip using local storage

--- script.js
+++ script.js
@@ -10,3 +10,11 @@ document.getElementById('new-tip').addEventListener('click', () => {
   const randomTip = tips[Math.floor(Math.random() * tips.length)];
   document.getElementById('tip').textContent = randomTip;
 });
+const tipEl = document.getElementById('tip');
+tipEl.textContent = localStorage.getItem('lastTip') || tipEl.textContent;
+
+document.getElementById('new-tip').onclick = () => {
+  const tip = tips[Math.floor(Math.random() * tips.length)];
+  tipEl.textContent = tip;
+  localStorage.setItem('lastTip', tip);
+};
